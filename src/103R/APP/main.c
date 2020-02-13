18ae34ce83aad616b40b7645cbc7ffd3605d2a0e
qitas <qitas@qitas.cn>
2020-02-13 19:13:32 +0800

    add src stm32f103r

---
+++ src/103R/APP/main.c
@@ -0,0 +1,71 @@
+
+#include "stm32f10x.h"
+#include "string.h"    
+#include <stdio.h>
+#include <stdlib.h>
+
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+}; 
+
+FILE __stdout;       
+//定义_sys_exit()以避免使用半主机模式    
+void _sys_exit(int x) 
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{      
+	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
+    USART1->DR = (uint8_t) ch;      
+	return ch;
+}
+#endif 
+
+
+void uart_init(u32 bound){
+    //GPIO端口设置
+    GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA, ENABLE);	//使能USART1，GPIOA时钟
+ 	USART_DeInit(USART1);  //复位串口1
+	 //USART1_TX   PA.9
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA.9
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
+    GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化PA9
+   
+    //USART1_RX	  PA.10
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
+    GPIO_Init(GPIOA, &GPIO_InitStructure);  //初始化PA10
+
+   //Usart1 NVIC 配置
+
+	USART_InitStructure.USART_BaudRate = bound;//一般设置为9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+
+	USART_Init(USART1, &USART_InitStructure); //初始化串口
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启中断
+	USART_Cmd(USART1, ENABLE);                    //使能串口 
+
+}
+int main(void)
+{
+    uart_init(115200);	 	        //串口初始化为115200
+    printf("\r\n ############test###########");
+    while(1)
+    {
+		}
+}
+
